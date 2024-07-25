5350a028bf7eceb85bb6047d3dae8c5f28773998
Dmitry Kazmerchuk <dkazmerchuk@ittula.ru>
2024-07-25 20:59:47 +0300

    Initial commit

---
+++ src/main/java/ru/netology/sqlhibernate/SqlHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.sqlhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlHibernateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/netology/sqlhibernate/entity/Person.java
@@ -0,0 +1,33 @@
+package ru.netology.sqlhibernate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PERSONS")
+public class Person {
+    @EmbeddedId
+    private PersonId personId;
+
+    @Column(name = "phone_number", nullable = false, length = 254)
+    private String phoneNumber;
+
+    @Column(name = "city_of_living", nullable = false, length = 254)
+    private String city;
+
+    @Override
+    public String toString() {
+        return "\nPerson {" + personId
+                + ", phoneNumber= " + phoneNumber
+                + ", city= " + city
+                + "}";
+    }
+}
